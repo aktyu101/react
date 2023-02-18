13bac18d6641e8f8c9e55dd9be389cc100cee944
aktyu101 <aktyu101@gmail.com>
2023-02-18 12:09:53 +0900

    230218 study

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import "./App.css";
 import React from "react";
 import Button from "./Button";
 // import { Props } from "./study/Props";
-import Condition from "./study/Condition";
+//import Condition from "./study/Condition";
+import Loop from "./study/Loop";
 
 function App() {
   return (
     <>
       <div className="app-wrapper">
-        <div className="app">
-          <Condition />
-        </div>
+        <Loop />
       </div>
     </>
   );
